836011fc4dafffea8ff832a50091bae873bdd656
jewoo <jewoos@gmail.com>
2022-11-25 14:15:53 +0900

    page 267

--- vm.c
+++ vm.c
@@ -8,6 +8,7 @@
 #include "debug.h"
 #include "vm.h"
 
+
 VM vm;
 
 static void resetStack() {
@@ -25,6 +26,7 @@ void freeVM() {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) do { double b = pop(); double a = pop(); push(a op b); }while(false)
 
     for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -47,6 +49,18 @@ static InterpretResult run() {
                 printf("\n");
                 return INTERPRET_OK;
             }
+            case OP_ADD:
+                BINARY_OP(+);
+                break;
+            case OP_SUBTRACT:
+                BINARY_OP(-);
+                break;
+            case OP_MULTIPLY:
+                BINARY_OP(*);
+                break;
+            case OP_DIVIDE:
+                BINARY_OP(/);
+                break;
             case OP_NEGATE:
                 push(-pop());
                 break;
@@ -60,6 +74,7 @@ static InterpretResult run() {
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk *chunk) {
