c17960fc917e63fd6811599a50150f887558c33f
jewoo <jewoos@gmail.com>
2022-11-28 15:33:32 +0900

    page 351

--- table.h
+++ table.h
@@ -23,4 +23,10 @@ void initTable(Table *table);
 
 void freeTable(Table *table);
 
+bool tableGet(Table *table, ObjString *key, Value *value);
+
+bool tableSet(Table *table, ObjString *key, Value value);
+
+void tableAddAll(Table *from, Table *to);
+
 #endif //CRAFTING_TABLE_H
