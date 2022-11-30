3eed305aa08dada01a15e856e00f5d34ec287fcc
jewoo <jewoos@gmail.com>
2022-11-30 13:31:27 +0900

    page 359

--- table.c
+++ table.c
@@ -137,3 +137,27 @@ bool tableDelete(Table *table, ObjString *key) {
     entry->value = BOOL_VAL(true);
     return true;
 }
+
+ObjString *tableFindString(Table *table, const char *chars, int length, uint32_t hash) {
+    if (table->count == 0) {
+        return NULL;
+    }
+
+    uint32_t index = hash % table->capacity;
+    for (;;) {
+        Entry *entry = &table->entries[index];
+        if (entry->key == NULL) {
+            // Stop if we find an empty non-tombstone entry;
+            if (IS_NIL(entry->value)) {
+                return NULL;
+            }
+        } else if (entry->key->length == length &&
+                   entry->key->hash == hash &&
+                   memcpy(entry->key->chars, chars, length) == 0) {
+
+            // we found it.
+            return entry->key;
+        }
+        index = (index + 1) % table->capacity;
+    }
+}
--- table.h
+++ table.h
@@ -31,4 +31,8 @@ bool tableDelete(Table *table, ObjString *key);
 
 void tableAddAll(Table *from, Table *to);
 
+ObjString *tableFindString(Table *table,
+                           const char *chars,
+                           int length, uint32_t hash);
+
 #endif //CRAFTING_TABLE_H
