02c5e135a0eddc676ca185a9f549ed3717cfe839
jewoo <jewoos@gmail.com>
2022-12-03 16:34:59 +0900

    page 437

--- memory.c
+++ memory.c
@@ -34,6 +34,9 @@ static void freeObject(Obj *object) {
             FREE(ObjFunction, object);
             break;
         }
+        case OBJ_NATIVE:
+            FREE(ObjNative, object);
+            break;
     }
 }
 
