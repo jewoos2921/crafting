f490048acf489c4eb6620e1cf7e1b900999978d0
jewoo <jewoos@gmail.com>
2022-12-31 01:10:41 +0900

    os IA-32e 모드 수정 1

--- 64OS/Kernel32/Source/Main.c
+++ 64OS/Kernel32/Source/Main.c
@@ -4,6 +4,8 @@
 // C 코드 엔트리 포인트 파일
 #include "Types.h"
 #include "Page.h"
+// 보호 모드 커널의 C언어 엔트리 포인트
+#include "ModeSwitch.h"
 
 void kPrintString(int iX, int iY, const char *pcString);
 
@@ -14,8 +16,12 @@ BOOL kIsMemoryEnough(void);
 
 int main() {
     DWORD i;
+    DWORD dwEAX, dwEBX, dwECX, dwEDX;
+
+    char vcVendorString[13] = {0,};
 
     kPrintString(0, 3, "C Language Kernel Started~!!!");
+
 
     // 최소 메모리 크기를 만족하는 지 검사
     kPrintString(0, 4, "Minimum Memory Size Check....................[      ]");
@@ -44,6 +50,31 @@ int main() {
     kInitializePageTables();
     kPrintString(45, 6, "Pass");
 
+    // 프로세스 제조사 정보 읽기
+    kReadCPUID(0x00, &dwEAX, &dwEBX, &dwECX, &dwEDX);
+    *(DWORD *) vcVendorString = dwEBX;
+    *((DWORD *) vcVendorString + 1) = dwEDX;
+    *((DWORD *) vcVendorString + 2) = dwECX;
+
+    kPrintString(0, 7, "Processor vendor string ......... [          ]");
+    kPrintString(45, 7, vcVendorString);
+
+    // 64 비트 지원 유무 확인
+    kReadCPUID(0x80000001, &dwEAX, &dwEBX, &dwECX, &dwEDX);
+    kPrintString(0, 8, "64bit Mode Support Check .............[         ]");
+
+    if (dwEDX & (1 << 29)) {
+        kPrintString(45, 8, "Pass");
+    } else {
+        kPrintString(45, 8, "Fail");
+        kPrintString(0, 9, "This processor does not support 64bit model~!!");
+        while (1);
+    }
+
+    // IA-32e 모드로 전환
+    kPrintString(0, 9, "Switch to IA-32e Mode");
+    // 원래는 아래 함수를 호출해야 하나 IA-32e 모드 커널이 없으므로 주석 처리
+//    kSwitchAndExecute64bitKernel();
 
     while (1);
 }
