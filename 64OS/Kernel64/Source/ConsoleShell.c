33a7d17e0817048357a538935dc99b110faecbc8
jewoo <jewoos@gmail.com>
2023-01-13 13:58:39 +0900

    코솔셸 추가

--- 64OS/Kernel64/Source/ConsoleShell.c
+++ 64OS/Kernel64/Source/ConsoleShell.c
@@ -26,6 +26,63 @@ SHELL_COMMAND_ENTRY gs_vstCommnadTable[] = {
         {"createtask", "Create Task",                                                 kCreateTestTask},
 };
 
+//======================================================================================================================
+// 실제 셸을 구성하는 코드
+//======================================================================================================================
+// 셸의 메인 루프
+void kStartConsoleShell(void) {
+    char vcCommandBuffer[CONSOLE_SHELL_MAX_COMMAND_BUFFER_COUNT];
+    int iCommandBufferIndex = 0;
+    BYTE bKey;
+    int iCursorX, iCursorY;
+    // 프롬프트 출력
+    kPrintf(CONSOLE_SHELL_PROMPT_MESSAGE);
+
+    while (1) {
+        // 키가 수신될 때까지 대기
+        bKey = kGetCh();
+        // Backspace 키 처리
+        if (bKey == KEY_BACKSPACE) {
+            if (iCommandBufferIndex > 0) {
+
+
+                // 현재 커서 위치를 얻어서 한 문자 앞으로 이동한 다음 공백을 출력하고
+                // 커맨드 버퍼에서 마지막 문자 삭제
+                kGetCursor(&iCursorX, &iCursorY);
+                kPrintStringXY(iCursorX - 1, iCursorY, " ");
+                kSetCursor(iCursorX - 1, iCursorY);
+                iCommandBufferIndex--;
+            }
+        }
+            // 엔터 키 처리
+        else if (bKey == KEY_ENTER) {
+            kPrintf("\n");
+            if (iCommandBufferIndex > 0) {
+                // 커맨드 버퍼에 있는 명령을 실행
+                vcCommandBuffer[iCommandBufferIndex] = '\0';
+                kExecuteCommand(vcCommandBuffer);
+            }
+            // 프롬프트 출력 및 커맨드 버퍼 초기화
+            kPrintf("%s", CONSOLE_SHELL_PROMPT_MESSAGE);
+            kMemSet(vcCommandBuffer, '\0', CONSOLE_SHELL_MAX_COMMAND_BUFFER_COUNT);
+            iCommandBufferIndex = 0;
+        } else if ((bKey == KEY_LSHIFT) || (bKey == KEY_RSHIFT) || (bKey == KEY_CAPSLOCK) || (bKey == KEY_NUMLOCK) ||
+                   (bKey == KEY_SCROLL_LOCK)) { ;
+        } else {
+            // TAB은 공백으로
+            if (bKey == KEY_TAB) {
+                bKey = ' ';
+            }
+
+            // 버퍼에 공간이 남아 있을 때만 가능
+            if (iCommandBufferIndex < CONSOLE_SHELL_MAX_COMMAND_BUFFER_COUNT) {
+                vcCommandBuffer[iCommandBufferIndex++] = bKey;
+                kPrintf("%c", bKey);
+            }
+        }
+    }
+}
+
 
 // PIT 컨트롤러의 카운터 0 설정
 void kSetTimer(const char *pcParameterBuffer) {
@@ -53,6 +110,156 @@ void kSetTimer(const char *pcParameterBuffer) {
 
     kInitializePIT(MSTOCOUNT(lValue), bPeriodic);
     kPrintf("Time = %d ms, Periodic = %d Change Complete\n", lValue, bPeriodic);
+}
+
+// 커맨드 버퍼에 있는 커맨드를 비교하여 해당 커맨드를 처리
+void kExecuteCommand(const char *pcCommandBuffer) {
+    int i, iSpaceIndex;
+    int iCommandBufferLength, iCommandLength;
+    int iCount;
+
+    // 공백으로 구분된 커맨드를 추출
+    iCommandBufferLength = kStrLen(pcCommandBuffer);
+    for (iSpaceIndex = 0; iSpaceIndex < iCommandBufferLength; iSpaceIndex++) {
+        if (pcCommandBuffer[iSpaceIndex] == ' ') { break; }
+    }
+
+    // 커맨드 테이블을 검사해서 같은 이름의 커맨드가 있는지 확인
+    iCount = sizeof(gs_vstCommnadTable) / sizeof(SHELL_COMMAND_ENTRY);
+    for (i = 0; i < iCount; i++) {
+        iCommandLength = kStrLen(gs_vstCommnadTable[i].pcCommand);
+        // 커맨드의 길이와 내용이 완전히 일치하는지 검사
+        if ((iCommandLength == iSpaceIndex) && (kMemCmp(gs_vstCommnadTable[i].pcCommand, pcCommandBuffer,
+                                                        iSpaceIndex) == 0)) {
+            gs_vstCommnadTable[i].pfFunction(pcCommandBuffer + iSpaceIndex + 1);
+            break;
+        }
+    }
+    // 리스트에서 찾을 수 없다면 에러 출력
+    if (i >= iCount) {
+        kPrintf("'%s' is not found.\n", pcCommandBuffer);
+    }
+}
+
+// 파라미터 자료구조를 초기화
+void kInitializeParameter(PARAMETER_LIST *pstList, const char *pcParameter) {
+    pstList->pcBuffer = pcParameter;
+    pstList->iLength = kStrLen(pcParameter);
+    pstList->iCurrentPosition = 0;
+}
+
+// 공백으로 구분된 파라미터의 내용과 길이를 반환
+int kGetNextParameter(PARAMETER_LIST *pstList, char *pcParameter) {
+    int i;
+    int iLength;
+
+    // 더 이상 파리미터가 없으면 나감
+    if (pstList->iLength <= pstList->iCurrentPosition) { return 0; }
+
+    // 버퍼의 길이만큼 이동하면서 공백을 검색
+    for (i = pstList->iCurrentPosition; i < pstList->iLength; i++) {
+        if (pstList->pcBuffer[i] == ' ') { break; }
+    }
+
+    // 파라미터를 복사하고 길이를 반환
+    kMemCpy(pcParameter, pstList->pcBuffer + pstList->iCurrentPosition, i);
+    iLength = i - pstList->iCurrentPosition;
+    pcParameter[iLength] = '\0';
+
+    // 파라미터의 위치 업데이트
+    pstList->iCurrentPosition += iLength + 1;
+    return iLength;
+}
+
+//======================================================================================================================
+// 커맨드를 처리하는 함수
+//======================================================================================================================
+// 셸 도움말을 출력
+void kHelp(const char *pcPrameterBuffer) {
+    int i;
+    int iCount;
+    int iCursorX;
+    int iCursorY;
+    int iLength;
+    int iMaxCommandLength = 0;
+
+    kPrintf("============================================================================\n");
+    kPrintf("                                 MINT64 Shell Help                            ");
+    kPrintf("============================================================================\n");
+
+    iCount = sizeof(gs_vstCommnadTable) / sizeof(SHELL_COMMAND_ENTRY);
+
+    // 가장 긴 커맨드의 길이를 계산
+    for (i = 0; i < iCount; ++i) {
+        iLength = kStrLen(gs_vstCommnadTable[i].pcCommand);
+        if (iLength > iMaxCommandLength) {
+            iMaxCommandLength = iLength;
+        }
+    }
+
+    // 도움말 출력
+    for (i = 0; i < iCount; ++i) {
+        kPrintf("%s", gs_vstCommnadTable[i].pcCommand);
+        kGetCursor(&iCursorX, &iCursorY);
+        kSetCursor(iMaxCommandLength, iCursorY);
+        kPrintf(" - %s\n", gs_vstCommnadTable[i].pcHelp);
+    }
+}
+
+// 화면을 지움
+void kCls(const char *pcPrameterBuffer) {
+    // 맨 윗줄은 디버깅 용으로 사용하므로 화면을 지훈 후 라인 1로 커서 이동
+    kClearScrren();
+    kSetCursor(0, 1);
+}
+
+// 총 메모리 크기를 출력
+void kShowTotalRAMSize(const char *pcPrameterBuffer) {
+    kPrintf("Total RAM Size = %d MB\n", kGetTotalRAMSize());
+}
+
+// 문자열로 된 숫저룰 슛자로 변환하여 화면에 출력
+void kStringToDecimalHexTest(const char *pcPrameterBuffer) {
+    char vcParameter[100];
+    int iLength;
+    PARAMETER_LIST stList;
+    int iCount = 0;
+    long lValue;
+
+    // 파라미터 초기화
+    kInitializeParameter(&stList, pcPrameterBuffer);
+
+    while (1) {
+        // 다음 파라미터를 구함,
+        // 파라미터의 길이가 0이면 파아미터가 없는 것으므로 종료
+        iLength = kGetNextParameter(&stList, vcParameter);
+        if (iLength == 0) {
+            break;
+        }
+
+        //파라미터에 대한 정보를 출력하고 16진수인지 10진수인지 판단하여 변환한 후 결과를 printf 로 출력
+        kPrintf("Param %d = '%s' Length = %d,  ", iCount + 1, vcParameter, iLength);
+
+        // 0x로 시작하면 16진수 , 그외에는 10진수로 판단
+        if (kMemCmp(vcParameter, "0x", 2) == 0) {
+            lValue = kAToI(vcParameter + 2, 16);
+            kPrintf("HEX Value = %q\n", lValue);
+        } else {
+            lValue = kAToI(vcParameter, 10);
+            kPrintf("Decimal Value = %d\n", lValue);
+        }
+        iCount++;
+    }
+}
+
+// PC를 재시작
+void kShutdown(const char *pcPrameterBuffer) {
+    kPrintf("System Shutdown Start...\n");
+
+    // 키보드 컨트롤러를 통해 PC를 재시작
+    kPrintf("Press Any Key To Rebbot PC...");
+    kGetCh();
+    kReboot();
 }
 
 // PIT 컨트롤러를 직접 사용하여 ms 동안 대기
@@ -151,20 +358,117 @@ static void kTestTask(void) {
     }
 }
 
+
+// 화면 테두리를 돌면서 문자를 출력
+void kTestTask1(void) {
+    BYTE bData;
+    int i = 0;
+    int iX = 0;
+    int iY = 0;
+    int iMargin;
+
+    CHARACTER *pstScreen = (CHARACTER *) CONSOLE_VIDEO_MEMORY_ADDRESS;
+    TCB *pstRunningTask;
+
+    // 자신의 ID를 얻어서 화면 오프셋으로 사용
+    pstRunningTask = kGetRunningTask();
+    iMargin = (pstRunningTask->stLink.qwID & 0xFFFFFFFF) % 10;
+
+    // 화면 네 귀퉁이를 돌면서 문자 출력
+    while (1) {
+        switch (i) {
+            case 0:
+                iX++;
+                if (iX >= (CONSOLE_WIDTH - iMargin)) {
+                    i = 1;
+                }
+                break;
+            case 1:
+                iY++;
+                if (iY >= (CONSOLE_HEIGHT - iMargin)) {
+                    i = 2;
+                }
+                break;
+            case 2:
+                iX--;
+                if (iX < iMargin) {
+                    i = 3;
+                }
+                break;
+            case 3:
+                iY--;
+                if (iY < iMargin) {
+                    i = 0;
+                }
+                break;
+        }
+        // 문자 및 색깔 지정
+        pstScreen[iY * CONSOLE_WIDTH + iX].bCharactor = bData;
+        pstScreen[iY * CONSOLE_WIDTH + iX].bAttribute = bData & 0x0F;
+        bData++;
+
+        // 다른 태스크로 전환
+        kSchedule();
+    }
+}
+
+// 자신의 ID를 참고하여 특정 위치에 회전하는 바람개비를 출력
+void kTestTask2(void) {
+    int i = 0;
+    int iOffset;
+    CHARACTER *pstScreen = (CHARACTER *) CONSOLE_VIDEO_MEMORY_ADDRESS;
+    TCB *pstRunningTask;
+    char vcData[4] = {'-', '\\', '|', '/'};
+
+    // 자신의 ID를 얻어서 화면 오프셋으로 사용
+    pstRunningTask = kGetRunningTask();
+    iOffset = (pstRunningTask->stLink.qwID & 0xFFFFFFFF) * 2;
+    iOffset = CONSOLE_WIDTH * CONSOLE_HEIGHT - (iOffset % (CONSOLE_WIDTH * CONSOLE_HEIGHT));
+
+    while (1) {
+        // 회전하는 바람개비 표시
+        pstScreen[iOffset].bCharactor = vcData[i % 4];
+        // 색깔 지정
+        pstScreen[iOffset].bAttribute = (iOffset % 15) + 1;
+        i++;
+
+        // 다른 태스크로 전환
+        kSchedule();
+    }
+}
+
 // 테스크를 생성해서 멀티 스태킹 수행
 void kCreateTestTask(const char *pcParameterBuffer) {
-    KEYDATA stData;
-    int i = 0;
 
-    // 태스크 설정
-    kSetUpTask(&(gs_vstTask[1]), 1, 0, (QWORD) kTestTask, &(gs_vstStack),
-               sizeof(gs_vstStack));
+    PARAMETER_LIST stList;
+    char vcType[30];
+    char vcCount[30];
+    int i;
 
-    // 'q' 키가 입력되지 않을 때까지 수행
-    while (1) {
-        kPrintf("[%d] This message is from kConsoleShell. Press any key to switch TestTask~!!\n", i++);
-        if (kGetCh() == 'q') { break; }
-        // 위에서 키가 입력되면 태스크를 전환
-        kSwitchContext(&(gs_vstTask[0].stContext), &(gs_vstTask[1].stContext));
+    // 파라미터를 출력
+    kInitializeParameter(&stList, pcParameterBuffer);
+    kGetNextParameter(&stList, vcType);
+    kGetNextParameter(&stList, vcCount);
+
+    switch (kAToI(vcType, 10)) {
+
+        case 1:
+            for (i = 0; i < kAToI(vcCount, 10); i++) {
+                if (kCreateTask(0, (QWORD) kTestTask1) == NIL) {
+                    break;
+                }
+            }
+            kPrintf("Task1 %d Crated\n", i);
+            break;
+
+        case 2:
+        default:
+            for (i = 0; i < kAToI(vcCount, 10); i++) {
+                if (kCreateTask(0, (QWORD) kTestTask2) == NIL) {
+                    break;
+                }
+            }
+            kPrintf("Task2 %d Crated\n", i);
+            break;
     }
 }
